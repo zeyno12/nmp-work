408785df4366d1618903cab73c770e2d81bc7137
zeyno12 <zeynabbagirova1@gmail.com>
2022-06-13 23:38:30 +0400

    finiw npm

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,47 @@ var swiper = new Swiper(".mySwiper", {
   });
 
   
-  var swiper = new Swiper1(".mySwiper1", {
+  var swiper = new Swiper(".swiperzey", {
+    
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
-      el: ".swiper-pagination1",
+      el: ".swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next1",
-      prevEl: ".swiper-button-prev1",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      390: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
     },
   });
-  var swiper = new Swiper2(".mySwiper2", {
-        pagination: {
-          el: ".swiper-pagination2",
-          type: "progressbar2",
-        },
-        navigation: {
-          nextEl: ".swiper-button-next2",
-          prevEl: ".swiper-button-prev2",
-        },
-      });
+  
+  var swiper = new Swiper(".zeynuw", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      390: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+    },
+  });
